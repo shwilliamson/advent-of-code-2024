2ff21e6b9c492da3dc3021c8fabfcdc9dc73c5e8
Stuart Williamson <stuartw@phase2online.com>
2024-12-03 00:45:16 -0600

    day 3 part 2

--- aoc/day03.py
+++ aoc/day03.py
@@ -7,6 +7,11 @@ class Symbol:
     M = 'm'
     U = 'u'
     L = 'l'
+    D = 'd'
+    O = 'o'
+    N = 'n'
+    T = 't'
+    APOSTROPHE = '\''
     OPEN_PARENTHESIS = '('
     CLOSED_PARENTHESIS = ')'
     COMMA = ','
@@ -18,6 +23,9 @@ class StateMachine:
 
     def __init__(self):
         self.state = Symbol.START
+        self.enabled = True
+        self.enabling = False
+        self.disabling = False
         self.operand1 = ""
         self.operand2 = ""
 
@@ -29,10 +37,28 @@ class StateMachine:
         (Symbol.L, Symbol.OPEN_PARENTHESIS),
     ]
 
+    ENABLE_EDGES = [
+        (Symbol.END, Symbol.D),
+        (Symbol.START, Symbol.D),
+        (Symbol.D, Symbol.O),
+        (Symbol.O, Symbol.OPEN_PARENTHESIS),
+    ]
+
+    DISABLE_EDGES = [
+        (Symbol.END, Symbol.D),
+        (Symbol.START, Symbol.D),
+        (Symbol.D, Symbol.O),
+        (Symbol.O, Symbol.N),
+        (Symbol.N, Symbol.APOSTROPHE),
+        (Symbol.APOSTROPHE, Symbol.T),
+        (Symbol.T, Symbol.OPEN_PARENTHESIS),
+    ]
+
     def read(self, char: str) -> str:
         print(f"{self.state} -> {char}")
         # big ole switch statement, fun with compilers
         match (self.state, char):
+            # parse mul(123,456)
             case n if n in self.PREFIX_EDGES:
                 self.state = char
             case n if n[0] == Symbol.OPEN_PARENTHESIS and n[1] in DIGITS:
@@ -49,29 +75,55 @@ class StateMachine:
                 self.operand2 += char
             case n if n == (Symbol.OPERAND2, Symbol.CLOSED_PARENTHESIS) and len(self.operand2) > 0:
                 self.state = Symbol.END
+
+            # parse do() and don't()
+            case n if n in self.ENABLE_EDGES:
+                self.enabling = True
+                self.disabling = False
+                self.state = char
+            case n if n in self.DISABLE_EDGES:
+                self.enabling = False
+                self.disabling = True
+                self.state = char
+            case (Symbol.OPEN_PARENTHESIS, Symbol.CLOSED_PARENTHESIS):
+                if self.enabling:
+                    self.enabled = True
+                    print(f"State machine enabled.")
+                elif self.disabling:
+                    self.enabled = False
+                    print(f"State machine disabled.")
+                self.reset()
+
+            # illegal state (gibberish)
             case _:
                 print(f"illegal edge: {self.state} -> {char}")
                 self.reset()
+
         return self.state
+
 
     def reset(self):
         self.state = Symbol.START
         self.operand1 = ""
         self.operand2 = ""
+        self.enabling = False
+        self.disabling = False
 
 
     def compute(self) -> int:
+        if not self.enabled:
+            return 0
         return int(self.operand1) * int(self.operand2)
 
 
 def main():
     answer = 0
-    compiler = StateMachine()
+    parser = StateMachine()
     with open('resources/day03.txt', 'r') as file:
         for line in file:
             for char in line:
-                if compiler.read(char) == Symbol.END:
-                    answer += compiler.compute()
+                if parser.read(char) == Symbol.END:
+                    answer += parser.compute()
 
     print(f"The answer is {answer}")
 
