81542ea5d36f7e51d4ddf5c992dd755486c5d529
Stuart Williamson <stuartw@phase2online.com>
2024-12-14 23:27:54 -0600

    day13, pt2 - wrong answer, but I'm not sure why, it's the exact same as pt1

---
+++ aoc/day13-pt2.py
@@ -0,0 +1,50 @@
+import re
+from typing import Optional
+
+import numpy as np
+from numpy.linalg import inv
+
+
+def to_positive_whole(x: float) -> Optional[int]:
+    # verify to 4 decimal places
+    x = round(x, 4)
+    return int(x) if x > 0 and float(int(x)) == x else None
+
+
+def main():
+    A = [[0, 0], [0, 0]]
+    b = [0, 0]
+    total_cost = 0
+    with open('resources/day13.txt', 'r') as file:
+        lines = [line.strip() for line in file]
+        for idx, row in enumerate(lines):
+            match idx % 4:
+                case 0:
+                    match = re.match(r"Button A: X\+(\d+), Y\+(\d+)", row)
+                    A[0][0] = int(match.group(1))
+                    A[1][0] = int(match.group(2))
+                case 1:
+                    match = re.match(r"Button B: X\+(\d+), Y\+(\d+)", row)
+                    A[0][1] = int(match.group(1))
+                    A[1][1] = int(match.group(2))
+                case 2:
+                    match = re.match(r"Prize: X=(\d+), Y=(\d+)", row)
+                    b[0] = int(match.group(1)) + 10000000000000
+                    b[1] = int(match.group(2)) + 10000000000000
+                    try:
+                        x = np.dot(inv(np.array(A)), np.array(b))
+                        x0 = to_positive_whole(x[0])
+                        x1 = to_positive_whole(x[1])
+                        if x0 and x1:
+                            total_cost += (3 * x0) + x1
+                    except np.linalg.LinAlgError:
+                        print("error")
+                case 3:
+                    A = [[0, 0], [0, 0]]
+                    b = [0, 0]
+
+        print(f"answer is {total_cost}")
+
+
+if __name__ == '__main__':
+    main()
